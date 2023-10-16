49402af664d056ca19ef0606fd086893b6d03825
aguspranyoto <agusprnyt@gmail.com>
2023-10-16 09:34:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <section>Hero</section>
+      <section>Paralax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </>
+  );
+}
+
+export default App;
